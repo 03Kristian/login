2e1a5d4999af171d22f44641165c5ae61c854d58
Alejandra <machate47@misena.edu.co>
2022-08-04 21:30:18 -0500

    v1

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Unique;
+use App\Models\User;
+use Hash;
+use Validator;
+use Auth;
+use Illuminate\Support\Facades\Storage;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('Auth.login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function login(Request $request){
+        $validar=Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+        ]);
+        $credenciales=[
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ];
+        if (!$validar->fails()) {
+            $user=User::where('email','=',$request->email)->first();
+            if(Hash::check($user->password,$request->email)){
+              if(Auth::attempt($credenciales)){
+                    return view('dashboard');
+              }
+            }
+        }
+        else return("error");
+    }
+
+    public function registrar(){
+        return view('Auth.registro');
+    }
+
+    public function registrarse(Request $request){
+        $validar=Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required',
+            'password'=>'required',
+            'avatar'=>'required',
+        ]);
+        if(!$validar->fails()) {
+        $user =new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = hash::make($request->password);
+        // usamos una funcionalidad para validar del tipo de file sea adecuado
+        if($request->hasfile('avatar')){
+            $user->avatar = $request->file('avatar')->store('img','public');
+        }
+        $user->save();
+        return redirect()->route('user');    
+    }
+        else return("error");
+    }
+}
---
+++ resources/views/Auth/login.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="{{asset}}"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+        .contenedor{
+            background-image: url('public/img/wallpaper_login.jpg');
+            /* background-repeat:repeat-x; */
+    }
+    </style>
+
+</head>
+
+<body class="contenedor">
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <div class=" col-2 container-fluid pt-4 card shadow-lg" style="background-color: rgb(15, 221, 228);">
+        <form action="{{route('login')}}" method="post">
+            {{csrf_field()}}
+            <h3 class="text-center">Long in</h3>
+            <div class="form-group">
+                <label for="">Nombre</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+                <label for="">Email</label>
+                <input type="email" class="form-control" id="email" placeholder="email">
+            </div>
+            <div class="form-group">
+                <label for=""">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            {{-- <div class="form-group">
+                <label for="">Verifique su Password</label>
+                <input type="password" class="form-control" id="v_password" placeholder="v_password">
+              </div> --}}
+              <div class="form-group">
+            <button type="submit" class="btn btn-primary btn btn-outline-primary">continuar</button>
+        </div>
+            <script>
+                Swal.fire({
+                    position: 'top-center',
+                    icon: 'success',
+                    title: 'Login',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+            </script>
+        </form>
+    </div>
+
+</body>
+
+</html>
